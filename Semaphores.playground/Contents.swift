20e1700ebab20f9efa2f36c98b843bef442d15ce
ricardohtduarte <ricardo.duarte@mindera.com>
2020-11-01 20:30:17 +0000

    Add tasks in semaphores

--- Semaphores.playground/Contents.swift
+++ Semaphores.playground/Contents.swift
@@ -1,6 +1,25 @@
 import UIKit
 
 // SEMAPHORES
-// Controll how many threads have access to the same resource
+
+// Control how many threads have access to the same resource
+
+let semaphore = DispatchSemaphore(value: 2)
+let group = DispatchGroup()
+let queue = DispatchQueue(label: "com.test.semaphore")
+
+for i in 1...10 {
+    queue.async(group: group) {
+        semaphore.wait()
+        group.enter()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5, execute: {
+            defer {
+                group.leave()
+                semaphore.signal()
+            }
+            print("Task \(i) completed")
+        })
+    }
+}
 
 
